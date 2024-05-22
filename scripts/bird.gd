9a18e3c9f54f3bf29dd6813539c1f8dfdfc71ac3
dompsz <dompszcz@gmail.com>
2024-05-22 17:24:39 +0200

    Initial commit

---
+++ scripts/bird.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
